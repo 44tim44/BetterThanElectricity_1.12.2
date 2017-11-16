5b1315058529963f85862b9f38a63e1313da1b98
Timeout <timeisrelative94@gmail.com>
2017-11-16 18:03:31 +0100

    Added hitboxes to cables

--- src/main/java/se/sst_55t/betterthanelectricity/block/cable/BlockCable.java
+++ src/main/java/se/sst_55t/betterthanelectricity/block/cable/BlockCable.java
@@ -1,13 +1,18 @@
 package se.sst_55t.betterthanelectricity.block.cable;
 
+import com.google.common.collect.Lists;
 import net.minecraft.block.Block;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraft.block.material.Material;
@@ -23,6 +28,7 @@ import se.sst_55t.betterthanelectricity.block.solarpanel.BlockSolarPanel;
 import se.sst_55t.betterthanelectricity.block.windmill.BlockWindMill;
 
 import javax.annotation.Nullable;
+import java.util.List;
 
 /**
  * Created by Timeout on 2017-08-22.
@@ -37,6 +43,15 @@ public class BlockCable extends BlockTileEntity<TileEntityCable>
     public static final PropertyBool UP = PropertyBool.create("up");
     public static final PropertyBool DOWN = PropertyBool.create("down");
 
+    //                                                                  X0          Y0          Z0         X1          Y2          Z2
+    public static final AxisAlignedBB AABB_CORE =    new AxisAlignedBB(0.3125D,     0.3125D,    0.3125D,   0.6875D,    0.6875D,    0.6875D);
+    public static final AxisAlignedBB AABB_SOUTH =   new AxisAlignedBB(0.3125D,     0.3125D,    0.6875D,   0.6875D,    0.6875D,    1.0D);
+    public static final AxisAlignedBB AABB_WEST =    new AxisAlignedBB(0.0D,        0.3125D,    0.3125D,   0.3125D,    0.6875D,    0.6875D);
+    public static final AxisAlignedBB AABB_NORTH =   new AxisAlignedBB(0.3125D,     0.3125D,    0.0D,      0.6875D,    0.6875D,    0.3125D);
+    public static final AxisAlignedBB AABB_EAST =    new AxisAlignedBB(0.6875D,     0.3125D,    0.3125D,   1.0D,       0.6875D,    0.6875D);
+    public static final AxisAlignedBB AABB_UP =      new AxisAlignedBB(0.3125D,     0.6875D,    0.3125D,   0.6875D,    1.0D,       0.6875D);
+    public static final AxisAlignedBB AABB_DOWN =    new AxisAlignedBB(0.3125D,     0.0D,       0.3125D,   0.6875D,    0.3125D,    0.6875D);
+
     public int color;
 
     public BlockCable(String name, int color)
@@ -45,6 +60,132 @@ public class BlockCable extends BlockTileEntity<TileEntityCable>
         this.setHardness(1.0F);
         this.color = color;
         this.setDefaultState(this.blockState.getBaseState().withProperty(NORTH, Boolean.valueOf(false)).withProperty(EAST, Boolean.valueOf(false)).withProperty(SOUTH, Boolean.valueOf(false)).withProperty(WEST, Boolean.valueOf(false)).withProperty(UP, Boolean.valueOf(false)).withProperty(DOWN, Boolean.valueOf(false)));
+    }
+
+    @Override
+    public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, @Nullable Entity entityIn, boolean p_185477_7_)
+    {
+        addCollisionBoxToList(pos, entityBox, collidingBoxes, AABB_CORE);
+        if(state.getValue(NORTH))
+        {
+            addCollisionBoxToList(pos, entityBox, collidingBoxes, AABB_NORTH);
+        }
+        if(state.getValue(SOUTH))
+        {
+            addCollisionBoxToList(pos, entityBox, collidingBoxes, AABB_SOUTH);
+        }
+        if(state.getValue(WEST))
+        {
+            addCollisionBoxToList(pos, entityBox, collidingBoxes, AABB_WEST);
+        }
+        if(state.getValue(EAST))
+        {
+            addCollisionBoxToList(pos, entityBox, collidingBoxes, AABB_EAST);
+        }
+        if(state.getValue(UP))
+        {
+            addCollisionBoxToList(pos, entityBox, collidingBoxes, AABB_UP);
+        }
+        if(state.getValue(DOWN))
+        {
+            addCollisionBoxToList(pos, entityBox, collidingBoxes, AABB_DOWN);
+        }
+    }
+
+    private static List<AxisAlignedBB> getCollisionBoxList(IBlockState state)
+    {
+        List<AxisAlignedBB> list = Lists.<AxisAlignedBB>newArrayList();
+        list.add(AABB_CORE);
+        if(state.getValue(NORTH))
+        {
+            list.add(AABB_NORTH);
+        }
+        if(state.getValue(SOUTH))
+        {
+            list.add(AABB_SOUTH);
+        }
+        if(state.getValue(WEST))
+        {
+            list.add(AABB_WEST);
+        }
+        if(state.getValue(EAST))
+        {
+            list.add(AABB_EAST);
+        }
+        if(state.getValue(UP))
+        {
+            list.add(AABB_UP);
+        }
+        if(state.getValue(DOWN))
+        {
+            list.add(AABB_DOWN);
+        }
+        return list;
+    }
+
+    @Override
+    public AxisAlignedBB getSelectedBoundingBox(IBlockState state, World worldIn, BlockPos pos) {
+        /*
+        if(state.getValue(NORTH))
+        {
+            return FULL_BLOCK_AABB.offset(pos);
+        }
+        if(state.getValue(SOUTH))
+        {
+            return FULL_BLOCK_AABB.offset(pos);
+        }
+        if(state.getValue(WEST))
+        {
+            return FULL_BLOCK_AABB.offset(pos);
+        }
+        if(state.getValue(EAST))
+        {
+            return FULL_BLOCK_AABB.offset(pos);
+        }
+        if(state.getValue(UP))
+        {
+            return FULL_BLOCK_AABB.offset(pos);
+        }
+        if(state.getValue(DOWN))
+        {
+            return FULL_BLOCK_AABB.offset(pos);
+        }
+        */
+        return AABB_CORE.offset(pos);
+    }
+
+    /**
+     * Ray traces through the blocks collision from start vector to end vector returning a ray trace hit.
+     */
+    @Override
+    @Nullable
+    public RayTraceResult collisionRayTrace(IBlockState blockState, World worldIn, BlockPos pos, Vec3d start, Vec3d end)
+    {
+        List<RayTraceResult> list = Lists.<RayTraceResult>newArrayList();
+
+        for (AxisAlignedBB axisalignedbb : getCollisionBoxList(this.getActualState(blockState, worldIn, pos)))
+        {
+            list.add(this.rayTrace(pos, start, end, axisalignedbb));
+        }
+
+        RayTraceResult raytraceresult1 = null;
+        double d1 = 0.0D;
+
+        for (RayTraceResult raytraceresult : list)
+        {
+            if (raytraceresult != null)
+            {
+                double d0 = raytraceresult.hitVec.squareDistanceTo(end);
+
+                if (d0 > d1)
+                {
+                    raytraceresult1 = raytraceresult;
+                    d1 = d0;
+                }
+            }
+        }
+
+        return raytraceresult1;
     }
 
     /**
